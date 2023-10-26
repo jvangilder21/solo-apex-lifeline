45b76d99d483b7a684a38622da65d48d61a77621
Jenny Van Gilder <jkosty21@gmail.com>
2023-10-25 20:10:34 -0500

    moved the stats loading from the info to the add stats page

--- src/components/AddStats/AddStats.jsx
+++ src/components/AddStats/AddStats.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 
 // Basic functional component structure for React with default state
@@ -14,12 +15,48 @@ function AddStats(props) {
 
   const [heading, setHeading] = useState('CHOOSE YOUR STATS');
 
+    // const[theStats, setTheStats] = useState([]);
+  const[theStats, setTheStats] = useState({});
+
   const OverallStats = (event) => {
     history.push('/overallStats')
     }
 
+  useEffect(() => {
+    fetchStats();
+    // fetchOrder();
+  }, []);
+
+  const fetchOrder = () => {
+    axios.get('/stats').then((response) => {
+      if (response.data.length > 0) {
+        fetchStats();
+      } else {
+        alert('Please select stats!');
+      }
+    })
+  }
+
+   const fetchStats = () => {
+    console.log("Running FetchStats");
+    axios.get('/info')
+    .then((response) => {
+      const apiResponse = response.data;
+      const totalData = response.data.total;
+
+      console.log('API response', apiResponse.data)
+      console.log('totalData log', totalData)
+      setTheStats(totalData)
+      // setTheStats(totalData);
+      // console.log();
+    }).catch((error) => {
+      console.log('axios get error', error);
+    })
+  }
+
+  console.log('theStats log', theStats);
   return (
-    <div>
+    <div className="container">
       <h2>{heading}</h2>
 
       <button className="btnStats">KD</button>
@@ -36,9 +73,33 @@ function AddStats(props) {
       <br/>
       <br/>
       <br/>
-      <button className="btn"
-      onClick={OverallStats}>BACK</button>
-    </div>
+
+      <button className="btn" onClick={OverallStats}>BACK</button>
+
+      <br/>
+      <br/>
+      <h4><i>STATS LOADING</i></h4>
+
+      {/* Are the stats defined? Do they have at least one property? */}
+
+      <div>
+          <p>Kills: {theStats?.kills?.value || 'Loading...'}</p>
+          <p>Headshots: {theStats?.headshots?.value || 'Loading...'}</p>
+          <p>Damage: {theStats?.damage?.value || 'Loading...'}</p>
+          <p>Executions: {theStats?.executions?.value || 'Loading...'}</p>
+          <p>Revives: {theStats?.revives?.value || 'Loading...'}</p>
+          <p>KD: {theStats?.kd?.value || 'Loading...'}</p>
+      </div>
+
+
+      
+      {/* {theStats.map((stat, index) => (
+        <p key={index}>{stat.total}</p>
+        ))} */}
+
+  </div>
+
+
   );
 }
 
--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -11,66 +11,11 @@ function InfoPage() {
 
   const [heading, setHeading] = useState('RANKED BREAKDOWN');
 
-  // const[theStats, setTheStats] = useState([]);
-  const[theStats, setTheStats] = useState({});
-
-
-  useEffect(() => {
-    fetchStats();
-    // fetchOrder();
-  }, []);
-
-  const fetchOrder = () => {
-    axios.get('/stats').then((response) => {
-      if (response.data.length > 0) {
-        fetchStats();
-      } else {
-        alert('Please select stats!');
-      }
-    })
-  }
-
-  const fetchStats = () => {
-    console.log("Running FetchStats");
-    axios.get('/info')
-    .then((response) => {
-      const apiResponse = response.data;
-      const totalData = response.data.total;
-
-      console.log('API response', apiResponse.data)
-      console.log('totalData log', totalData)
-      setTheStats(totalData)
-      // setTheStats(totalData);
-      // console.log();
-    }).catch((error) => {
-      console.log('axios get error', error);
-    })
-  }
-  
-    console.log('theStats log', theStats);
   return (
     <div className="container">
       <h2>{heading}</h2>
-      <br/>
-      <br/>
-      <h4><i>STATS LOADING</i></h4>
-
-{/* Are the stats defined? Do they have at least one property? */}
-
-      <div>
-        <p>Kills: {theStats?.kills?.value || 'Loading...'}</p>
-        <p>Headshots: {theStats?.headshots?.value}</p>
-        <p>Damage: {theStats?.damage?.value}</p>
-        <p>Executions: {theStats?.executions?.value}</p>
-        <p>Revives: {theStats?.revives?.value}</p>
-        <p>KD: {theStats?.kd?.value}</p>
-      </div>
-
-
      
-        {/* {theStats.map((stat, index) => (
-        <p key={index}>{stat.total}</p>
-      ))} */}
+
 
     </div>
   ); 
