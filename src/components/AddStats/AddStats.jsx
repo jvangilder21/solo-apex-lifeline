1e642792eed239c4f70068b465eeab99ebd849ff
Jenny Van Gilder <jkosty21@gmail.com>
2023-10-26 20:24:02 -0500

    the AddStats component now select toggle which stat to save to the overall Stats

--- src/components/AddStats/AddStats.jsx
+++ src/components/AddStats/AddStats.jsx
@@ -17,25 +17,40 @@ function AddStats(props) {
 
     // const[theStats, setTheStats] = useState([]);
   const[theStats, setTheStats] = useState({});
+  const [selectedStats, setSelectedStats] = useState([]);
 
-  const OverallStats = (event) => {
+  const backToOverallStats = (event) => {
     history.push('/overallStats')
-    }
+  }
+
+  const saveToOverallStats = () => {
+    history.push('/overallStats', {selectedStats})
+  }
+  
+
+  const handleStatSelection = (stat) => {
+    // setSelectedStats((prevSelectedStats) => {
+      if (selectedStats.includes(stat)) {
+        setSelectedStats(selectedStats.filter((selectedStat) => selectedStat !== stat));
+      } else {
+        setSelectedStats([...selectedStats, stat]);
+      }
+  };
 
   useEffect(() => {
     fetchStats();
     // fetchOrder();
   }, []);
 
-  const fetchOrder = () => {
-    axios.get('/stats').then((response) => {
-      if (response.data.length > 0) {
-        fetchStats();
-      } else {
-        alert('Please select stats!');
-      }
-    })
-  }
+  // const fetchOrder = () => {
+  //   axios.get('/stats').then((response) => {
+  //     if (response.data.length > 0) {
+  //       fetchStats();
+  //     } else {
+  //       alert('Please select stats!');
+  //     }
+  //   })
+  // }
 
    const fetchStats = () => {
     console.log("Running FetchStats");
@@ -55,26 +70,35 @@ function AddStats(props) {
   }
 
   console.log('theStats log', theStats);
+
   return (
     <div className="container">
       <h2>{heading}</h2>
 
-      <button className="btnStats">KD</button>
-      <button className="btnStats">REVIVES</button>
-      <button className="btnStats">WINS</button>
-      <button className="btnStats">KNOCKDOWNS</button>
+
+      <button className="btnStats" onClick={() => handleStatSelection('KILLS')}>KILLS</button>
+      <button className="btnStats" onClick={() => handleStatSelection('HEADSHOTS')}>HEADSHOTS</button>
+      <button className="btnStats" onClick={() => handleStatSelection('DAMAGE')}>DAMAGE</button>
       <br/>
       <br/>
-      <button className="btnStats">ASSISTS</button>
-      <button className="btnStats">DAMAGE DEALT</button>
-      <button className="btnStats">FINISHERS</button>
-      <button className="btnStats">HEADSHOTS</button>
-      <br/>
-      <br/>
+      <button className="btnStats" onClick={() => handleStatSelection('EXECUTIONS')}>EXECUTIONS</button>
+      <button className="btnStats" onClick={() => handleStatSelection('REVIVES')}>REVIVES</button>
+      <button className="btnStats" onClick={() => handleStatSelection('KD')}>KD</button>
+
       <br/>
       <br/>
 
-      <button className="btn" onClick={OverallStats}>BACK</button>
+      <button className="btn" onClick={backToOverallStats}>BACK</button>
+
+      <br/>
+      <br/>
+      <h4><i>SELECTED STATS:</i></h4>
+
+        {selectedStats.map((stat) => (
+        <p key={stat}>{stat}</p>
+        ))}
+
+      <button className="btn" onClick={saveToOverallStats}>SAVE</button>
 
       <br/>
       <br/>
@@ -91,11 +115,6 @@ function AddStats(props) {
           <p>KD: {theStats?.kd?.value || 'Loading...'}</p>
       </div>
 
-
-      
-      {/* {theStats.map((stat, index) => (
-        <p key={index}>{stat.total}</p>
-        ))} */}
 
   </div>
 
