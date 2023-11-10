4bf4e112ce516465d76f28720507cf1741a55326
Jenny Van Gilder <jkosty21@gmail.com>
2023-11-09 21:25:20 -0600

    Updated Landing Page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ import './LandingPage.css';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState('Welcome Apex Legends Gamers! ');
   const history = useHistory();
 
   const onLogin = (event) => {
@@ -17,39 +17,36 @@ function LandingPage() {
     <div className="container">
       <h2>{heading}</h2>
 
-      <div className="grid">
+      <div className="grid" >
         <div className="grid-col grid-col_8">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
+            Apex Lifeline is your go to app to track your in game stats
+            on the go. While also creating and tracking your challenes!
+          </p>
+          <br/>
+          <p>
+           Before registering, you will be able to pull your stats. Please 
+           make sure that you have at least one of the following trackers set as a 
+           tracker on your legend in Apex Legends to correctly pull your stats:
+           <ul>
+            <li>Kills</li>
+            <li>Headshots</li>
+            <li>Damage Dealt</li>
+            <li>Finishers</li>
+            <li>Revives</li>
+           </ul>
           </p>
 
           <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
+           Please register using your Origin username that is affiliated with
+           your gamer tag/ username. 
           </p>
 
+          <br/>
           <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
+          Good luck gamer!  
           </p>
+
         </div>
         <div className="grid-col grid-col_4">
           <RegisterForm />
